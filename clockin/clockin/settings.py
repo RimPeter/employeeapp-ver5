f13de4981b9869c0b219079ee494dc14b3fccecc
RimPeter <hull@localappliancerentals.co.uk>
2024-06-26 22:05:58 +0100

    DEBUG FALSE

--- clockin/clockin/settings.py
+++ clockin/clockin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m1qe7ladlhkn(tbx2z#06mam43tjaw&*!gs5cc@g%q5-*l_g+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['herokuapp.com', '127.0.0.1']
 
